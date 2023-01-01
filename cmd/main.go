90f5dfbfe3d2852641c9e010fa019b5fdf48675e
codeby-student <codeby-student@proton.me>
2023-01-01 00:03:00 +0000

    add jwt auth

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"crypto"
+	"fmt"
+	jwtMiddleware "github.com/codeby-student/go-service/pkg/middleware/jwt"
+	"github.com/codeby-student/go-service/pkg/utils"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	publicKey, err := os.ReadFile("production/public.key")
+	if err != nil {
+		log.Fatal(fmt.Errorf("error read public key: %w", err))
+	}
+	//privateKey, err := os.ReadFile("private.key")
+	//if err != nil {
+	//	log.Fatal(fmt.Errorf("error read private key: %w", err))
+	//}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	jwt := jwtMiddleware.Jwt(crypto.SHA512, publicKey)
+	r.Use(jwt)
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("It works!"))
-		if err != nil {
-			log.Print(err)
+		payload, ok := jwtMiddleware.FromContext(r.Context())
+		if !ok {
+			utils.WriteResponse(w, http.StatusUnauthorized, "err.auth_required")
+			return
 		}
+
+		utils.WriteResponse(w, http.StatusOK, fmt.Sprintf("Welcome, %s!", payload.Subject))
 	})
-	err := http.ListenAndServe(":8888", r)
+	err = http.ListenAndServe(":8888", r)
 	if err != nil {
-		log.Print(err)
+		log.Fatal(err)
 	}
 }
--- verify.sh
+++ verify.sh
@@ -4,4 +4,3 @@ set -eux;
 
 gpg --keyid-format long --verify SHA256SUMS.gpg SHA256SUMS;
 sha256sum -c SHA256SUMS;
-
